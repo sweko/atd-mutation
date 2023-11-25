352e45be3553e9656e8e76c04a28e2762860e9d5
Wekoslav Stefanovski <swekster@gmail.com>
2023-11-25 17:13:42 +0100

    Initial Commit

---
+++ code/Calculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code
+{
+    public class Calculator
+    {
+        public int Add(int first, int second)
+        {
+            return first + second;
+        }
+
+        public int Subtract(int first, int second)
+        {
+            return first + second;
+        }
+    }
+}
---
+++ test/CalculatorTest.cs
@@ -0,0 +1,13 @@
+using Code;
+
+namespace test
+{
+    public class CalculatorTest
+    {
+        [Fact]
+        public void Calculator_Add_Returns_Result()
+        {
+            var calc = new Calculator();
+        }
+    }
+}
